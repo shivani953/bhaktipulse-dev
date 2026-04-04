6c49e7ce1c5322946e2b9028708295133687eaba
Shivani <shivanikhammampati21@gmail.com>
2026-04-04 23:27:34 +0530

    first commit

---
+++ backend/public/script.js
@@ -0,0 +1,38 @@
+const today = new Date().toISOString().split("T")[0];
+document.getElementById("date").value = today;
+
+function submitData() {
+  let name = document.getElementById("name").value;
+  let count = document.getElementById("count").value;
+
+  if (!name || !count) {
+    alert("Fill all fields");
+    return;
+  }
+
+  fetch("/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      date: today,
+      count: count
+    })
+  })
+  .then(res => res.json())
+  .then(() => {
+    loadIndividual(name);
+
+    document.getElementById("name").value = "";
+    document.getElementById("count").value = "";
+  });
+}
+function loadIndividual(name) {
+  fetch("/user/" + name)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("individual").innerText = data.total;
+    });
+}
---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(__dirname + "/public"));
+
+let logs = [];
+const filePath = __dirname + "/data.json";
+if (fs.existsSync(filePath)) {
+  logs = JSON.parse(fs.readFileSync(filePath));
+}
+
+app.post("/submit", (req, res) => {
+  const { name, date, count } = req.body;
+
+  if (!name || !count) {
+    return res.status(400).json({ error: "Invalid input" });
+  }
+
+  logs.push({
+    name,
+    date,
+    count: Number(count)
+  });
+
+  fs.writeFileSync(filePath, JSON.stringify(logs, null, 2));
+
+  res.json({ message: "Saved successfully" });
+});
+
+
+app.get("/user/:name", (req, res) => {
+  const name = req.params.name;
+
+  const total = logs
+    .filter(log => log.name === name)
+    .reduce((sum, log) => sum + log.count, 0);
+
+  res.json({ total });
+});
+
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running");
+});
